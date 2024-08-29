6395a1662538db79e5e0a0cc9b22cb00fd119017
Edward Dudas <edwarddudas@gmail.com>
2024-08-29 16:14:23 +0300

    first commit

---
+++ dist/index.101921d5.js
@@ -0,0 +1,2 @@
+module.exports=JSON.parse('{"name":"feedback-form","version":"1.0.0","scripts":{"start":"node server.js","build":"parcel build src/index.html --dist-dir dist","dev":"parcel src/index.html --dist-dir dist"},"dependencies":{"express":"^4.19.2"},"devDependencies":{"parcel":"^2.8.3"}}');
+//# sourceMappingURL=index.101921d5.js.map
---
+++ dist/index.436fb39c.js
@@ -0,0 +1,2 @@
+document.getElementById("feedbackForm").addEventListener("submit",function(e){e.preventDefault(),console.log({name:document.getElementById("name").value,email:document.getElementById("email").value})});
+//# sourceMappingURL=index.436fb39c.js.map
